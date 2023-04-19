bf6f340b7ca515d4e1c2d1f74c8c0c29ccc8ee82
Matias Camiletti <matias.camiletti@gmail.com>
2023-04-19 14:42:40 -0300

    Create first structure for module.

---
+++ projects/tots/notification/src/lib/notification.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-notification',
+  template: `
+    <p>
+      notification works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NotificationComponent {
+
+}
---
+++ projects/tots/notification/src/lib/notification.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NotificationComponent } from './notification.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotificationComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NotificationComponent
+  ]
+})
+export class NotificationModule { }

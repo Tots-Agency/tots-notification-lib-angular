79076e6614f53445f3300638d8f2bdaaa2da47bd
Matias Camiletti <matias.camiletti@gmail.com>
2023-04-20 08:40:15 -0300

    Add public entity.

--- projects/tots/notification-core/src/public-api.ts
+++ projects/tots/notification-core/src/public-api.ts
@@ -2,5 +2,8 @@
  * Public API Surface of notification-core
  */
 
+/** Entities */
+export * from './lib/entities/tots-notification';
+
 /** Modules */
 export * from './lib/notification-core.module';

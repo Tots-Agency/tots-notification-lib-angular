42c3c728aafbbccddb4e0978c5b50b32b3a568ff
Matias Camiletti <matias.camiletti@gmail.com>
2023-04-20 11:49:31 -0300

    Add show avatar and letter avatar.

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -8,18 +8,26 @@ import { TotsNotification } from 'projects/tots/notification-core/src/public-api
 })
 export class AppComponent {
   notification = new TotsNotification();
+  notification2 = new TotsNotification();
 
   nots = new Array<TotsNotification>();
   isLoading = false;
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.notification.title = 'New Furniture Lead';
     this.notification.caption = 'A new furniture Lead #25 has been arrived and require review';
     this.notification.created_at = '2023-05-25 18:00:00';
     this.notification.photoTooltip = 'Sales Module';
     this.notification.photo = 'https://gentle-field-01a36a00f-development.eastus2.2.azurestaticapps.net/assets/img/icons-modules/016-stock exchange app.svg';
+    //this.notification.photoIsCircle = true;
+
+    this.notification2.title = 'New Furniture Lead';
+    this.notification2.caption = 'A new furniture Lead #25 has been arrived and require review';
+    this.notification2.created_at = '2023-05-25 18:00:00';
+    this.notification2.photoTooltip = 'Matias Camiletti';
+    this.notification2.photo = '';
+    this.notification2.photoIsCircle = true;
+    this.notification2.photoAvatarLetters = 'MC';
 
     let not2 = new TotsNotification();
     not2.title = 'New Design Lead';
@@ -32,6 +40,9 @@ export class AppComponent {
     this.nots.push(not2);
     this.nots.push(this.notification);
     this.nots.push(this.notification);
+    this.nots.push(not2);
+    this.nots.push(not2);
+    this.nots.push(not2);
     this.nots.push(not2);
   }
 }
--- projects/tots/notification/src/lib/components/tots-notification-style-one/tots-notification-style-one.component.ts
+++ projects/tots/notification/src/lib/components/tots-notification-style-one/tots-notification-style-one.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TotsNotification } from '@tots/notification-core';
 
 @Component({
@@ -6,10 +6,17 @@ import { TotsNotification } from '@tots/notification-core';
   templateUrl: './tots-notification-style-one.component.html',
   styleUrls: ['./tots-notification-style-one.component.scss']
 })
-export class TotsNotificationStyleOneComponent {
+export class TotsNotificationStyleOneComponent implements OnInit {
   @Input() notification!: TotsNotification;
   @Output() clickNot = new EventEmitter<TotsNotification>();
   @Output() readNot = new EventEmitter<TotsNotification>();
+
+  backgroundColors = ['#B9FF98', '#98FFE6', '#98A8FF', '#D06CFF', '#F56B8D', '#FFAF82', '#FFF282'];
+  randomNumber = 0;
+
+  ngOnInit(): void {
+    this.processRandomNumber();
+  }
 
   onCLickNotification() {
     this.notification.is_read = 1;
@@ -19,5 +26,27 @@ export class TotsNotificationStyleOneComponent {
   onClickRead() {
     this.notification.is_read = 1;
     this.readNot.emit(this.notification);
+  }
+
+  processRandomNumber() {
+    if(this.notification.photoAvatarLetters == undefined||this.notification.photoAvatarLetters == ''){
+      return;
+    }
+    this.randomNumber = this.getSameRandomNumberForLetters(this.notification.photoAvatarLetters);
+  }
+
+  getSameRandomNumberForLetters(str: string): number {
+    const seed = this.hash(str);
+    let x = Math.sin(seed) * 10000;
+    x = x - Math.floor(x);
+    return Math.floor(x * 7);
+  }
+
+  hash(s: string): number {
+    let h = 0;
+    for (let i = 0; i < s.length; i++) {
+      h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+    }
+    return h;
   }
 }
